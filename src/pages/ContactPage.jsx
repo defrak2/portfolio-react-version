ebc6a1d6a0a49cbfa3d4689948f8507801e79791
defrak2 <defranciscok22@gmail.com>
2024-09-25 09:51:44 -0400

    added information for the portfolio page

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,11 @@
+
+
+export default function ContactPage() {
+
+
+  return (
+    <div id="contact-page">
+      <h1>Contact Me</h1>
+    </div>
+  );
+}
---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,11 @@
+
+
+export default function ResumePage() {
+
+
+  return (
+    <div id="resume-page">
+      <h1>FIND RESUME HERE</h1>
+    </div>
+  );
+}
